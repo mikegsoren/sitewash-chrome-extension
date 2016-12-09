3de574a0aa2a38760384d0b23cb3c9a81616e709
Joseph Galindo <joseph.galindo18@gmail.com>
2016-12-09 15:30:10 -0500

    popup JS works, however a content script is needed to manipulate the DOM, which is now added and works. Next step is to get the two scripts talking to eachother

--- popup.js
+++ popup.js
@@ -1,21 +1,38 @@
-(function() {
-
-
-    console.log(document.getElementById("wash"));
-
-
-    var wash = function () {
-
-        console.log(chrome.extension.getBackgroundPage());
-
-        var washStyles = document.createElement('style');
-
-        chrome.extension.getBackgroundPage().document.write("<style>body { background-color:#000 }</style>");
-    };
+function createListener() {
 
     var washButton = document.getElementById('wash');
     washButton.addEventListener('click', function() {
         wash();
     }); 
+}
 
-})();
+function wash() {
+
+	var queryInfo = {
+	active: true,
+	currentWindow: true
+	};
+
+	var tab;
+
+	chrome.tabs.query(queryInfo, function(tabs) {
+		// chrome.tabs.query invokes the callback with a list of tabs that match the
+		// query. When the popup is opened, there is certainly a window and at least
+		// one tab, so we can safely assume that |tabs| is a non-empty array.
+		// A window can only have one active tab at a time, so the array consists of
+		// exactly one tab.
+		tab = tabs[0];
+
+		console.log(tab);
+
+	    console.log(chrome.extension.getBackgroundPage());
+
+	    var washStyles = document.createElement('style');
+
+	    chrome.extension.getBackgroundPage().document.write("<style>body { background-color:#000 }</style>");
+	});	
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    createListener();
+});
