530c9911ab9602b7d474f25afd640e26544f8977
msorensen <msorensen@cdmprinceton.com>
2016-12-12 15:26:26 -0500

    added some TODOs

--- dom-script.js
+++ dom-script.js
@@ -1,18 +1,35 @@
 (function() {
-	var textElements = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'a', 'li'];
 
-	for (var i = 0; i < textElements.length; i++) {
-    
-		var elements = document.getElementsByTagName(textElements[i]);
 
-		for (var j = 0; j < elements.length; j++) {
+	// TODO: make it so you can set a min size from popup.html, also add max font size set by user
+	var processFontSizes = function() {
+		var textElements = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'a', 'li'];
 
-			var fontSize = parseInt(window.getComputedStyle(elements[j]).getPropertyValue('font-size').replace('px', ''));
+		for (var i = 0; i < textElements.length; i++) {
+	    
+			var elements = document.getElementsByTagName(textElements[i]);
 
-			if (fontSize < 14) { 
-				elements[j].setAttribute('class', 'font-size-override'); 
+			for (var j = 0; j < elements.length; j++) {
+
+				var fontSize = parseInt(window.getComputedStyle(elements[j]).getPropertyValue('font-size').replace('px', ''));
+
+				if (fontSize < 14) { 
+					elements[j].setAttribute('class', 'font-size-override'); 
+				}
+
 			}
-
 		}
-	}
+	};
+
+
+	var unfixElements = function() {
+
+		// TODO: set fixed position elements to static if the popup option checkbox is checked
+
+	};
+
+
+
+	processFontSizes();
+
 })();
